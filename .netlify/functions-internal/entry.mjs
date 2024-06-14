047d93bea96085660ed7df524cc0ba76f6fbad3d
andrests <andresterraza.at@gmail.com>
2024-06-14 18:10:21 -0500

    Ultimo commit para hacerlo con netlify

--- .netlify/functions-internal/entry.mjs
+++ .netlify/functions-internal/entry.mjs
@@ -1,22 +1,12 @@
 import * as adapter from '@astrojs/netlify/netlify-functions.js';
 import { renderers } from './renderers.mjs';
-import { manifest } from './manifest_2211ba2e.mjs';
-import 'react';
-import 'react-dom/server';
-import 'cookie';
-import 'kleur/colors';
-import 'string-width';
-import 'html-escaper';
-import 'clsx';
-import './chunks/astro_3054e7fa.mjs';
-import 'mime';
-import 'path-to-regexp';
+import { manifest } from './manifest_00999142.mjs';
 
-const _page0  = () => import('./chunks/image-endpoint_3dc24b21.mjs');
-const _page1  = () => import('./chunks/index_c67b9eca.mjs');
-const _page2  = () => import('./chunks/services_0425eba8.mjs');
-const _page3  = () => import('./chunks/contact_0d81bc09.mjs');
-const _page4  = () => import('./chunks/about_e242606c.mjs');const pageMap = new Map([["node_modules/astro/dist/assets/image-endpoint.js", _page0],["src/pages/index.astro", _page1],["src/pages/services.astro", _page2],["src/pages/contact.astro", _page3],["src/pages/about.astro", _page4]]);
+const _page0  = () => import('./chunks/generic_3591afc5.mjs');
+const _page1  = () => import('./chunks/index_aefdc825.mjs');
+const _page2  = () => import('./chunks/services_80457165.mjs');
+const _page3  = () => import('./chunks/contact_854ff9c8.mjs');
+const _page4  = () => import('./chunks/about_4a70d757.mjs');const pageMap = new Map([["node_modules/astro/dist/assets/endpoint/generic.js", _page0],["src/pages/index.astro", _page1],["src/pages/services.astro", _page2],["src/pages/contact.astro", _page3],["src/pages/about.astro", _page4]]);
 const _manifest = Object.assign(manifest, {
 	pageMap,
 	renderers,
