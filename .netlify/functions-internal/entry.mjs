20f9d6db757f2f7baaf51629e1b58ac13540574f
andrests <andresterraza.at@gmail.com>
2024-06-16 20:51:31 -0500

    Solo cambiamos el endpoint

--- .netlify/functions-internal/entry.mjs
+++ .netlify/functions-internal/entry.mjs
@@ -6,7 +6,7 @@ const _page0  = () => import('./chunks/generic_3591afc5.mjs');
 const _page1  = () => import('./chunks/index_aefdc825.mjs');
 const _page2  = () => import('./chunks/services_80457165.mjs');
 const _page3  = () => import('./chunks/contact_854ff9c8.mjs');
-const _page4  = () => import('./chunks/about_4a70d757.mjs');const pageMap = new Map([["node_modules/astro/dist/assets/endpoint/generic.js", _page0],["src/pages/index.astro", _page1],["src/pages/services.astro", _page2],["src/pages/contact.astro", _page3],["src/pages/about.astro", _page4]]);
+const _page4  = () => import('./chunks/about_4a70d757.mjs');const pageMap = new Map([["node_modules/astro/dist/endpoint/generic.js", _page0],["src/pages/index.astro", _page1],["src/pages/services.astro", _page2],["src/pages/contact.astro", _page3],["src/pages/about.astro", _page4]]);
 const _manifest = Object.assign(manifest, {
 	pageMap,
 	renderers,
